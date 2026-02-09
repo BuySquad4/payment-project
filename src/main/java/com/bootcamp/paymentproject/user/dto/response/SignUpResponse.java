565e13892d661fd77a4dba81d23bfbe22760daa3
hyu1335 <hyuham1335@gmail.com>
2026-02-09 19:00:20 +0900

    fix: 회원가입 실패 오류 수정

--- src/main/java/com/bootcamp/paymentproject/user/dto/response/SignUpResponse.java
+++ src/main/java/com/bootcamp/paymentproject/user/dto/response/SignUpResponse.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 public class SignUpResponse {
-    private boolean success;
     private String name;
     private String phoneNumber;
     private String email;
