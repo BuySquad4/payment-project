a5ecc06fdea15b363e4dbf1529e3d04a3c9349f1
perfect-Bee <joonslee3@gmail.com>
2026-02-10 12:01:45 +0900

    add : ProductResponse 추가

--- src/main/java/com/bootcamp/paymentproject/product/dto/ProductResponse.java
+++ src/main/java/com/bootcamp/paymentproject/product/dto/ProductResponse.java
@@ -1,4 +1,44 @@
 package com.bootcamp.paymentproject.product.dto;
 
+
+import com.bootcamp.paymentproject.product.entity.Product;
+import com.bootcamp.paymentproject.product.enums.ProductStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
 public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Long stock;
+    private String description;
+    private ProductStatus status;
+    private String category;
+
+    public static ProductResponse from(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .description(product.getDescription())
+                .status(product.getStatus())
+                .category(product.getCategory())
+
+                .build();
+    }
 }
+
+// @Build, from
+//    private Long id;
+//    private String name;
+//    private BigDecimal price;
+//    private Long stock;
+//    private String description;
+//    private ProductStatus status;
+//    private String category;
